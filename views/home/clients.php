f9fc71305e537320f44a6c74202682da470627e8
ivanvillan <ivanvillan54@gmail.com>
2021-03-04 14:27:21 -0300

    seller|clients

--- views/home/clients.php
+++ views/home/clients.php
@@ -8,5 +8,87 @@
 </head>
 <body>
     <?php include('../header/header.php') ?>
+    <div class="container">
+        <div class="row">
+            <h4>CLIENTES</h4>
+            <div class="col s12 m4 l4">
+                <div class="input-field">
+                    <select name="" id="rep">
+                        <option value="0" disabled selected>Repartición</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="input-field">
+                    <select name="" id="state">
+                        <option value="0" disabled selected>Estado</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="input-field">
+                    <select name="" id="seller">
+                        <option value="0" disabled selected>Vendedor</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col s12 m12 l12">
+                <table class="highlight responsive-table">
+                    <thead>
+                        <tr>
+                            <th>Legajo</th>
+                            <th>Nombre</th>
+                            <th>DNI</th>
+                            <th>CBU</th>
+                            <th>Denominacion</th>
+                            <th>Información</th>
+                            <th>Movimientos</th>
+                            <th>Observaciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            getClients();
+        });
+        function getClients(){
+            $.ajax({
+                type: "GET",
+                url: "http://localhost/mutualasn-api/public/customers/get/all/all/all",
+                dataType: "json",
+                success: function (response) {
+                    console.log(response.result);
+                    let row = response.result;
+                    let html = [];
+                    for (let i=0; i < row.length; i++){
+                    html.push(
+                    `<tr>
+                    <td>${row[i].Legajo}</td> 
+                    <td>${row[i].ApellidoNombreC}</td> 
+                    <td>${row[i].DNI}</td> 
+                    <td>${row[i].CBU}</td> 
+                    <td>${row[i].Denominacion}</td> 
+                    <td> <button class="btn right"><i class="material-icons">description</i></button></td> 
+                    <td><button class="btn right"><i class="material-icons">assignment</i></button></td> 
+                    <td><button class="btn right"><i class="material-icons">library_books</i></button></td> 
+                    </tr>`
+                    );
+                }    
+                $('table>tbody').html(html.join(''));
+                $('select').formSelect();
+                },
+                error: function(){
+                    console.log('error');
+                }
+            });
+        }
+    </script>
 </body>
 </html>
--- views/home/seller.php
+++ views/home/seller.php
@@ -8,5 +8,75 @@
 </head>
 <body>
     <?php include('../header/header.php') ?>
+    <div class="container">
+        <div class="row">
+            <h4>VENDEDORES</h4>
+            <div class="input-field col s6">
+                <input id="search" type="text" class="validate">
+                <label for="search">Buscador...</label>
+            </div>
+            <div class="col s12 m12 l12">
+                <table class="highlight responsive-table">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Tel. Fijo</th>
+                            <th>Tel. Móvil</th>
+                            <th>Domicilio</th>
+                            <th>Correo</th>
+                            <th>Información</th>
+                            <th>Movimientos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            getSellers();
+            search();
+        });
+        function getSellers(){
+            $.ajax({
+                type: "GET",
+                url: "http://localhost/mutualasn-api/public/entities/get/sellers",
+                dataType: "json",
+                success: function (response) {
+                    console.log(response.result);
+                    let row = response.result;
+                    let html = [];
+                    for (let i=0; i < row.length; i++){
+                    html.push(
+                    `<tr class="content">
+                    <td>${row[i].ApellidoNombreV}</td> 
+                    <td>${row[i].TelFijo}</td> 
+                    <td>${row[i].TelMovil}</td> 
+                    <td>${row[i].Domicilio}</td> 
+                    <td>${row[i].CorreoElectronico}</td> 
+                    <td> <button class="btn right"><i class="material-icons">description</i></button></td> 
+                    <td><button class="btn right"><i class="material-icons">assignment</i></button></td> 
+                    </tr>`
+                    );
+                }    
+                $('table>tbody').html(html.join(''));
+                },
+                error: function(){
+                    console.log('error');
+                }
+            });
+        }
+        function search(){
+            $("#search").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $(".content").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        }
+    </script>
 </body>
 </html>
